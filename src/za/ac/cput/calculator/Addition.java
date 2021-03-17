ac78d1079fcc4f2c3aa9669d0e84467e5fb804b4
Ameer <220005060@mycput.ac.za>
2021-03-17 22:24:33 +0200

    Modified the integerSum() Added doubleSum

--- src/za/ac/cput/calculator/Addition.java
+++ src/za/ac/cput/calculator/Addition.java
@@ -10,9 +10,22 @@ package za.ac.cput.calculator;
  * @author Ameer
  */
 public class Addition {
-    
+    /**
+     * This adds two integers
+     * @param a
+     * @param b
+     * @return 
+     */
     private static int integersSum(int a, int b){
         return a + b;
     }
-    
+    /**
+     * This adds two real numbers
+     * @param a
+     * @param b
+     * @return 
+     */
+    public static double doubleSum(double a, double b){
+        return a + b;
+    }
 }
