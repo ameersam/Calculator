ed988dfed068046a162905ae701bdee9ffaa3f97
Ameer <220005060@mycput.ac.za>
2021-03-17 22:17:06 +0200

    First Commit

---
+++ src/za/ac/cput/calculator/Addition.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package za.ac.cput.calculator;
+
+/**
+ *
+ * @author Ameer
+ */
+public class Addition {
+    
+    private static int integersSum(int a, int b){
+        return a + b;
+    }
+    
+}
